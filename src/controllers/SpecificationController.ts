4419f81ba115755690ffd2cb970de0d1b91d6652
Willian K. Sanada <sanada@NOTE-PAT-082.local>
2022-07-12 18:18:26 -0300

    Commit parcial

--- src/controllers/SpecificationController.ts
+++ src/controllers/SpecificationController.ts
@@ -3,21 +3,21 @@ import { container } from "tsyringe";
 import { SpecificationService } from "../services/SpecificationService";
 
 class SpecificationController {
-    create(request: Request, response: Response): Response {
+    async create(request: Request, response: Response): Promise<Response> {
         const service = container.resolve(SpecificationService)
         const { name, description } = request.body
-        service.create({ name, description })
+        await service.create({ name, description })
         return response.status(201).send()
     }
-    getAll(request: Request, response: Response): Response {
+    async getAll(request: Request, response: Response): Promise<Response> {
         const service = container.resolve(SpecificationService)
-        const list = service.getAll()
+        const list = await service.getAll()
         return response.json(list)
     }
-    findByName(request: Request, response: Response): Response {
+    async findByName(request: Request, response: Response): Promise<Response> {
         const service = container.resolve(SpecificationService)
         const { name } = request.body
-        const item = service.findByName(name)
+        const item = await service.findByName(name)
         return response.json(item)
     }
 }
